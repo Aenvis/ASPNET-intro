cca7d107d2e5ee1594827032444ae3e40b8550a6
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-04-22 18:45:18 +0200

    Seed the database with test movie records

---
+++ Models/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using HelloWorld.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloWorld.Models
+{
+	public static class SeedData
+	{
+		public static void Initialize(IServiceProvider serviceProvider)
+		{
+			using (var context = new HelloWorldContext(
+				serviceProvider.GetRequiredService<DbContextOptions<HelloWorldContext>>()
+				))
+			{
+				// Look for any movies
+				if (context.Movie.Any()) return; // DB has been seeded
+
+				context.Movie.AddRange(
+					new Movie
+					{
+						Title = "The Batman",
+						ReleaseDate = DateTime.Parse("2022-03-04"),
+						Genre = "Action movie",
+						Price = 10.99M
+					},
+					new Movie 
+					{
+						Title = "Avatar: The Way of Water",
+						ReleaseDate = DateTime.Parse("2022-12-16"),
+						Genre = "Sci-fi",
+						Price = 53.12M
+					},
+					new Movie
+					{
+						Title = "Ghostbusters",
+						ReleaseDate = DateTime.Parse("1984-3-13"),
+						Genre = "Comedy",
+						Price = 8.99M
+					}
+					);
+
+				context.SaveChanges();
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using HelloWorld.Data;
+using HelloWorld.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HelloWorldContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HelloWorldContext") ?? throw new InvalidOperationException("Connection string 'HelloWorldContext' not found.")));
@@ -10,13 +11,20 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
 }
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
