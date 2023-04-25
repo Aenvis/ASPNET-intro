a54b95e5bade410158e56b865881586315599729
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-04-26 00:25:11 +0200

    Add Rating property to the Movie model. Migrate database to contain the new column and update the views

--- Migrations/20230425222049_Rating.Designer.cs
+++ Migrations/20230425222049_Rating.Designer.cs
@@ -4,6 +4,7 @@ using HelloWorld.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HelloWorld.Migrations
 {
     [DbContext(typeof(HelloWorldContext))]
-    partial class HelloWorldContextModelSnapshot : ModelSnapshot
+    [Migration("20230425222049_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -20,21 +20,24 @@ namespace HelloWorld.Models
 						Title = "The Batman",
 						ReleaseDate = DateTime.Parse("2022-03-04"),
 						Genre = "Action movie",
-						Price = 10.99M
+						Price = 10.99M,
+						Rating = "Excellent"
 					},
 					new Movie 
 					{
 						Title = "Avatar: The Way of Water",
 						ReleaseDate = DateTime.Parse("2022-12-16"),
 						Genre = "Sci-fi",
-						Price = 53.12M
+						Price = 53.12M,
+						Rating = "Ok"
 					},
 					new Movie
 					{
 						Title = "Ghostbusters",
 						ReleaseDate = DateTime.Parse("1984-3-13"),
 						Genre = "Comedy",
-						Price = 8.99M
+						Price = 8.99M,
+						Rating = "Good"
 					}
 					);
 
